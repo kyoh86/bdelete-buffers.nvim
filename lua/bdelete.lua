728d695993847023ce5e640eb1978123f98a61ad
kyoh86 <me@kyoh86.dev>
2026-06-09 13:54:16 +0900

    use vim.islist instead of vim.tbl_islist

--- lua/bdelete.lua
+++ lua/bdelete.lua
@@ -4,24 +4,24 @@
 ---@param default any @A default value.
 ---@return any
 local function get_param(params, key, default)
-    if params == nil or type(params) ~= "table" or vim.tbl_islist(params) then
-        return default
-    elseif not vim.fn.has_key(params, key) then
-        return default
-    end
-    return params[key]
+  if params == nil or type(params) ~= "table" or vim.islist(params) then
+    return default
+  elseif not vim.fn.has_key(params, key) then
+    return default
+  end
+  return params[key]
 end
 
 --- Normalize a parameter table.
 ---@param params bdelete.OptionalParams?
 ---@return bdelete.Params
 local function normalize_params(params)
-    return {
-        force = get_param(params, "force", false),
-        keep_layout = get_param(params, "keep_layout", false),
-        filter = get_param(params, "filter", nil),
-        debug = get_param(params, "debug", false),
-    }
+  return {
+    force = get_param(params, "force", false),
+    keep_layout = get_param(params, "keep_layout", false),
+    filter = get_param(params, "filter", nil),
+    debug = get_param(params, "debug", false),
+  }
 end
 
 local M = {}
@@ -32,43 +32,43 @@ local active = require("bdelete.active")
 --- Wipe buffers other of the one in the current window.
 ---@param params bdelete.OptionalParams?
 function M.other(params)
-    static.other(normalize_params(params))
+  static.other(normalize_params(params))
 end
 
 --- Wipe buffers loaded but currently not displayed in any window.
 ---@param params bdelete.OptionalParams?
 function M.hidden(params)
-    static.hidden(normalize_params(params))
+  static.hidden(normalize_params(params))
 end
 
 --- Wipe buffers having no name.
 ---@param params bdelete.OptionalParams?
 function M.nameless(params)
-    static.nameless(normalize_params(params))
+  static.nameless(normalize_params(params))
 end
 
 --- Wipe all buffers.
 ---@param params bdelete.OptionalParams?
 function M.all(params)
-    static.all(normalize_params(params))
+  static.all(normalize_params(params))
 end
 
 --- Wipe a buffer in the current window.
 ---@param params bdelete.OptionalParams?
 function M.current(params)
-    static.current(normalize_params(params))
+  static.current(normalize_params(params))
 end
 
 --- Open a menu to choose one of behaviors below.
 ---@param params bdelete.OptionalParams?
 function M.menu(params)
-    active.menu(normalize_params(params))
+  active.menu(normalize_params(params))
 end
 
 --- Lets you interactively select which buffers to wipe.
 ---@param params bdelete.OptionalParams?
 function M.select(params)
-    active.select(normalize_params(params))
+  active.select(normalize_params(params))
 end
 
 return M
